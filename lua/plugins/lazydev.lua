cb5645585d089f19325d07fc2507d42f6196ebd3
Atopes Sayuri <108107785+AtopesSayuri@users.noreply.github.com>
2024-06-03 00:01:35 +0800

    fix: lazydev config errs

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -1,14 +1,13 @@
 return {
-    {
-        "folke/lazydev.nvim",
-        ft = "lua", -- only load on lua files
-        opts = {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    opts = function()
+        return {
             library = {
-                vim.env.LAZY .. "/luvit-meta/library", -- see below
-                -- You can also add plugins you always want to have loaded.
-                -- Useful if the plugin has globals or types you want to use
-                -- vim.env.LAZY .. "/LazyVim", -- see below
+                uv = "luvit-meta/library",
+                lazyvim = "LazyVim",
             },
-        },
-    },
+        }
+    end,
 }
+
